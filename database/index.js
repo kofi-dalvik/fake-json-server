273fe8b1ba8f0a8a955ae02cb3d2131d3007a0f6
Dawuda Ahmed <kofidahmed@gmail.com>
2019-03-17 16:05:34 +0000

    modified to create and update configurations

--- database/index.js
+++ database/index.js
@@ -6,21 +6,62 @@ const normalizedPath = path.join(__dirname, 'snapshots');
 //finding the current snapshot
 let currentSnapshot = fs.readdirSync(normalizedPath).sort().pop();
 
+const saveConfig = (config) => {
+    fs.writeFile('./../config.json', config, function (err) {
+        if (err) throw err;
+    });
+}
+
+//update current snapshot to include new seeds
+let config = {
+    routes: []
+};
+
+try {
+    config = require('./config.json');
+} catch (error) {
+    saveConfig(JSON.stringify({routes: []}));
+}
+
+let thereIsAChange = false;
+
 let db = {};
+
+const seedsPath = path.join(__dirname, 'seeds');
 
 if (currentSnapshot) {
     //require current snapshot
     db = require('./snapshots/' + currentSnapshot);
-} else {
-    //require all seeds
-    const seedsPath = path.join(__dirname, 'seeds');
 
     fs.readdirSync(seedsPath).forEach(filename => {
         let schema = filename.split('.').shift();
+
+        if (config.routes.indexOf(schema) < 0) {
+            thereIsAChange = true;
+            config.routes.push(schema);
+            db[schema] = require('./seeds/' + filename)();
+        }
+    });
+} else {
+    //require all seeds
+    fs.readdirSync(seedsPath).forEach(filename => {
+        let schema = filename.split('.').shift();
+        config.routes.push(schema);
         db[schema] = require('./seeds/' + filename)();
     });
+
+    thereIsAChange = true;
 }
 
+if (thereIsAChange) {
+    //write config
+    config = JSON.stringify(config);
+    saveConfig(config)
+}
+
+
+
+//save routes to config settings
 module.exports = db;
 
 
